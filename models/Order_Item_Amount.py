5db14b1a1f8a4127f03d510cc95b8f24573adaac
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2024-11-18 01:17:07 +0800

    Update Order_Item_Amount.py

--- models/Order_Item_Amount.py
+++ models/Order_Item_Amount.py
@@ -1,5 +1,5 @@
 from . import db
 class Order_Item_Amount(db.Model):
-   __tablename__ = 'Order_Item_Amount'
+   __tablename__ = 'Order_item_amount'
    AID = db.Column(db.Integer, primary_key=True, foreign_key=True, nullable=False)
    OID = db.Column(db.Integer, foreign_key=True, nullable=False)
