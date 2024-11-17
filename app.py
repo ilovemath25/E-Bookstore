09a237d2a0b2c5dd714f8737512edc6c76bca5b4
ilovemath25 <w3573y.young@gmail.com>
2024-11-17 23:06:31 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from models import db
+from models.member import Members
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'YourSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ilovemath25@localhost:5432/ebookstore'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def index():
+   members = Members.query.all()
+   return render_template("main.html", members=members)
+if __name__=='__main__': app.run(debug=True, port=5000)
---
+++ models/member.py
@@ -0,0 +1,16 @@
+from . import db
+class Members(db.Model):
+   __tablename__ = 'Members'
+   MID = db.Column(db.Integer, primary_key=True, nullable=False)
+   F_name = db.Column(db.String(15), nullable=False)
+   L_name = db.Column(db.String(15))
+   Password = db.Column(db.String(15), nullable=False)
+   Gender = db.Column(db.String(10), db.Enum('male', 'female', 'other'))
+   Email = db.Column(db.String(100), unique=True, nullable=False)
+   Phone = db.Column(db.Integer)
+   Birth = db.Column(db.Date)
+   Address = db.Column(db.String(200))
+   Reg_date = db.Column(db.Date, nullable=False)
+   A_flag = db.Column(db.Boolean, nullable=False)
+   S_flag = db.Column(db.Boolean, nullable=False)
+   C_flag = db.Column(db.Boolean, nullable=False)
