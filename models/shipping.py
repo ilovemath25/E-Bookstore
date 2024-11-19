a6f560be0ad1409abfa1329dddf465ee224d1eba
Yang-jzz <henryyang2003121@gmail.com>
2024-11-19 14:02:34 +0800

    Create shipping.py

---
+++ models/shipping.py
@@ -0,0 +1,8 @@
+from app import db
+from .discount import Discount
+
+class Shipping(db.Model):
+    __tablename__ = 'shipping'
+
+    DID = db.Column(db.Integer, db.ForeignKey('discount.DID', onupdate='CASCADE', ondelete='CASCADE'), primary_key=True, nullable=False)
+    Min_Purchase = db.Column(db.Integer)
