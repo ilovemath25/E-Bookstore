0b1dc935322e67553bcd59b4b13f4629ade6b71f
ilovemath25 <w3573y.young@gmail.com>
2024-11-22 00:45:54 +0800

    modify member, database, gitignore

--- models/members.py
+++ models/members.py
@@ -7,7 +7,7 @@ class Members(db.Model):
    Password = db.Column(db.String(15), nullable=False)
    Gender = db.Column(db.String(10), db.Enum('male', 'female', 'other'))
    Email = db.Column(db.String(100), unique=True, nullable=False)
-   Phone = db.Column(db.Integer)
+   Phone = db.Column(db.Char)
    Birth = db.Column(db.Date)
    Address = db.Column(db.String(200))
    Reg_date = db.Column(db.Date, nullable=False)
--- models/shipping.py
+++
@@ -1,8 +0,0 @@
-from . import db
-from .discount import Discount
-
-class Shipping(db.Model):
-    __tablename__ = 'shipping'
-
-    DID = db.Column(db.Integer, db.ForeignKey('discount.DID', onupdate='CASCADE', ondelete='CASCADE'), primary_key=True, nullable=False)
-    Min_Purchase = db.Column(db.Integer)
