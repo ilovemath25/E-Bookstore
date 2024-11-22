c7007216b47dd8a3af8d1355a4a23d81c8a93023
ilovemath25 <w3573y.young@gmail.com>
2024-11-22 13:39:27 +0800

    modify member.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from models import db
-from models.members import Members
+from models.member import Member
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YourSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ilovemath25@localhost:5432/ebookstore'
@@ -9,6 +9,6 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-   members = Members.query.all()
+   members = Member.query.all()
    return render_template("main.html", members=members)
 if __name__=='__main__': app.run(debug=True, port=5000)
--- models/member.py
+++ models/member.py
@@ -7,7 +7,7 @@ class Member(db.Model):
    Password = db.Column(db.String(15), nullable=False)
    Gender = db.Column(db.String(10), db.Enum('male', 'female', 'other'))
    Email = db.Column(db.String(100), unique=True, nullable=False)
-   Phone = db.Column(db.Char)
+   Phone = db.Column(db.String(10))
    Birth = db.Column(db.Date)
    Address = db.Column(db.String(200))
    Reg_date = db.Column(db.Date, nullable=False)
