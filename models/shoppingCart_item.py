2384bb6285300a121d83621fa3734c2c16d2e8ad
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2024-11-23 08:23:54 +0800

    Modify shoppingCart_item.py

--- models/shoppingCart_item.py
+++ models/shoppingCart_item.py
@@ -2,5 +2,5 @@ from . import db
 class ShoppingCart_item(db.Model):
     __tablename__ = 'ShoppingCart_item'
     SCID = db.Column(db.Integer, primary_key = True, nullable = False)
-    CMID = db.Column(db.Integer, db.ForeignKey('Member.MID'))
+    CMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"))
     Tot_price = db.Column(db.Integer, nullable = False)
