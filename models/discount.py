f2e2e5839ac6db0ecda6f2afa366a7b789d71b98
Yang-jzz <henryyang2003121@gmail.com>
2024-11-23 13:51:55 +0800

    Update and rename Discount.py to discount.py

--- models/discount.py
+++ models/discount.py
@@ -1,8 +1,7 @@
 from . import db
-
 class Discount(db.Model):
     __tablename__ = 'Discount'
-    DID = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    DID = db.Column(db.Integer, primary_key=True, nullable=False)
     OID = db.Column(db.Integer, db.ForeignKey('Order.OID', onupdate="CASCADE", ondelete="CASCADE"))
     Disc_code = db.Column(db.String(20), unique=True, nullable=False)
     Disc_value = db.Column(db.Numeric, nullable=False)
@@ -10,5 +9,3 @@ class Discount(db.Model):
     Disc_name = db.Column(db.String(50), nullable=False)
     Policy_desc = db.Column(db.String(1000), nullable=False)
     Max_usage = db.Column(db.Integer)
-    seasoning = db.relationship("Seasoning", back_populates="discount", uselist=False)
-    shipping = db.relationship("Shipping", back_populates="discount", uselist=False)
