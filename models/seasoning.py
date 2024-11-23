e69fce92f3e2ec9139054e47191fc56da05b960b
Yang-jzz <henryyang2003121@gmail.com>
2024-11-23 13:53:17 +0800

    Update and rename Seasoning.py to seasoning.py

--- models/seasoning.py
+++ models/seasoning.py
@@ -1,5 +1,4 @@
 from . import db
-
 class Seasoning(db.Model):
     __tablename__ = 'Seasoning'
     DID = db.Column(db.Integer, db.ForeignKey('Discount.DID', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True, nullable=False)
@@ -8,4 +7,3 @@ class Seasoning(db.Model):
     __table_args__ = (
         db.CheckConstraint('Valid_to >= Valid_from', name='valid_date_range'),
     )
-    discount = db.relationship("Discount", back_populates="seasoning")
