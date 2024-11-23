c70739209a6d794625410175d4243cf114c9d76e
vintbry <vincentius8055@gmail.com>
2024-11-23 15:32:33 +0800

    create credit_card.py, item_line.py, review.py

---
+++ models/item_line.py
@@ -0,0 +1,12 @@
+from . import db
+from sqlalchemy import Enum
+
+class ItemLine(db.Model):
+    __tablename__ = 'Item_line'
+    
+    LID = db.Column(db.Integer, primary_key=True, nullable=False)
+    PID = db.Column(db.Integer, db.ForeignKey('Product.PID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    OID = db.Column(db.Integer, db.ForeignKey('Order.OID', onupdate="CASCADE", ondelete="SET NULL")) 
+    SCID = db.Column(db.Integer, db.ForeignKey('ShoppingCart.SCID', onupdate="CASCADE", ondelete="SET NULL")) 
+    Line_type = db.Column(Enum('Order', 'ShoppingCart', name='line_type_enum'), nullable=False) 
+    Quantity = db.Column(db.Integer, nullable=False) 
