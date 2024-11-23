890d6f6fb83cdb12fdc0d6d7b8fe0e09c4b13abd
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2024-11-23 08:32:06 +0800

    Modify special_event.py

--- models/special_event.py
+++ models/special_event.py
@@ -1,7 +1,7 @@
 from . import db
 class Special_event(db.Model):
    __tablename__ = 'Special_event'
-   DID = db.Column(db.Integer, db.ForeignKey('Discount.DID'), primary_key=True, nullable=False)
+   DID = db.Column(db.Integer, db.ForeignKey('Discount.DID', onupdate="CASCADE", ondelete="CASCADE"), primary_key=True, nullable=False)
    Valid_to = db.Column(db.Date, nullable=False)
    Valid_from = db.Column(db.Date, nullable=False)
    __table_args__ = (db.CheckConstraint('Valid_to >= Valid_from', name='valid_date_range'))
