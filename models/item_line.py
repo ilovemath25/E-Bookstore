c17ea58eaa60457ed926605e631161737c599c44
ilovemath25 <147607832+ilovemath25@users.noreply.github.com>
2024-11-24 10:57:40 +0800

    Update item_line.py

--- models/item_line.py
+++ models/item_line.py
@@ -7,5 +7,5 @@ class Item_line(db.Model):
     PID = db.Column(db.Integer, db.ForeignKey('Product.PID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     OID = db.Column(db.Integer, db.ForeignKey('Order.OID', onupdate="CASCADE", ondelete="SET NULL")) 
     SCID = db.Column(db.Integer, db.ForeignKey('ShoppingCart.SCID', onupdate="CASCADE", ondelete="SET NULL")) 
-    Line_type = db.Enum('Order', 'ShoppingCart', name='line_type_enum'), nullable=False) 
+    Line_type = db.Column(db.Enum('Order', 'ShoppingCart'), nullable=False) 
     Quantity = db.Column(db.Integer, nullable=False) 
