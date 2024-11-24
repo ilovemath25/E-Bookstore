70a6d0ba0551b7fa752e11bceb8824585a654e32
ilovemath25 <w3573y.young@gmail.com>
2024-11-24 23:34:52 +0800

    organize code structure

---
+++ ebookstore_flask/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from ebookstore_flask.models import db
+def create_app(postgres):
+   app = Flask(__name__)
+   app.config['SECRET_KEY'] = 'YourSecretKey'
+   app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{postgres['user']}:{postgres['password']}@{postgres['host']}:{postgres['port']}/{postgres['db']}"
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   db.init_app(app)
+   with app.app_context():
+      from ebookstore_flask.routes.home import home
+      app.register_blueprint(home)
+   return app
