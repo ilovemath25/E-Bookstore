8cef4cd4b6e5153d08521aaef749266dc2ebc1b9
vintbry <vincentius8055@gmail.com>
2024-11-29 20:13:29 +0800

    Update item_line.py
    change db.enum

--- models/item_line.py
+++ models/item_line.py
@@ -1,11 +1,11 @@
 from . import db
 
-class Item_line(db.Model):
+class ItemLine(db.Model):
     __tablename__ = 'Item_line'
     
     LID = db.Column(db.Integer, primary_key=True, nullable=False)
     PID = db.Column(db.Integer, db.ForeignKey('Product.PID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
     OID = db.Column(db.Integer, db.ForeignKey('Order.OID', onupdate="CASCADE", ondelete="SET NULL")) 
     SCID = db.Column(db.Integer, db.ForeignKey('ShoppingCart.SCID', onupdate="CASCADE", ondelete="SET NULL")) 
-    Line_type = db.Enum('Order', 'ShoppingCart', name='line_type_enum'), nullable=False) 
+    Line_type = db.Column(db.Enum('Order', 'ShoppingCart', name='line_type_enum'), nullable=False) 
     Quantity = db.Column(db.Integer, nullable=False) 
