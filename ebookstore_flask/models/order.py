d526f577f9a2aeb35dc119a773df38c986718a1b
jeon0121 <jamie0121304@gmail.com>
2024-11-29 18:43:16 +0800

    add DID into order.py

--- ebookstore_flask/models/order.py
+++ ebookstore_flask/models/order.py
@@ -4,6 +4,7 @@ class Order(db.Model):
    OID = db.Column(db.Integer, primary_key=True, nullable=False)
    CMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
    SMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+   DID = db.Column(db.Integer, db.ForeignKey('Discount.DID', onupdate="CASCADE", ondelete="CASCADE"))
    Credit_num = db.Column(db.String(16), db.ForeignKey('Credit_card.Number', onupdate="CASCADE", ondelete="CASCADE"))
    Time = db.Column(db.DateTime, nullable=False)
    Ship_address = db.Column(db.String(200), nullable=False)
