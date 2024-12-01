427d1ef48cd1f7c7f61d237f21c8941fad1f659c
ilovemath25 <w3573y.young@gmail.com>
2024-12-01 14:49:31 +0800

    modify discount.py

--- ebookstore_flask/models/discount.py
+++ ebookstore_flask/models/discount.py
@@ -1,10 +1,11 @@
 from . import db
+
 class Discount(db.Model):
     __tablename__ = 'Discount'
     DID = db.Column(db.Integer, primary_key=True, nullable=False)
     Disc_code = db.Column(db.String(20), unique=True, nullable=False)
     Disc_value = db.Column(db.Numeric, nullable=False)
-    Disc_type = db.Column(db.Enum('Shipping', 'Seasonings', 'Special Events'), nullable=False)
+    Disc_type = db.Column(db.Enum('Shipping', 'Seasoning', 'Special Event'), nullable=False)
     Disc_name = db.Column(db.String(50), nullable=False)
     Policy_desc = db.Column(db.String(1000), nullable=False)
     Max_usage = db.Column(db.Integer)
