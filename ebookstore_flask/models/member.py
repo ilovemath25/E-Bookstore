e128f181dcf6d151dd77e7ee5e882d9314cbcb1c
ilovemath25 <w3573y.young@gmail.com>
2024-12-01 14:54:55 +0800

    modify member.py

--- ebookstore_flask/models/member.py
+++ ebookstore_flask/models/member.py
@@ -1,13 +1,14 @@
 from . import db
+
 class Member(db.Model):
    __tablename__ = 'Member'
    MID = db.Column(db.Integer, primary_key=True, nullable=False)
-   F_name = db.Column(db.String(15), nullable=False)
-   L_name = db.Column(db.String(15))
+   F_name = db.Column(db.String(20), nullable=False)
+   L_name = db.Column(db.String(20))
    Password = db.Column(db.String(15), nullable=False)
-   Gender = db.Column(db.String(10), db.Enum('male', 'female', 'other'))
+   Gender = db.Column(db.Enum('Male', 'Female', 'Other'), nullable=False)
    Email = db.Column(db.String(100), unique=True, nullable=False)
-   Phone = db.Column(db.String(10))
+   Phone = db.Column(db.String(10), nullable=False)
    Birth = db.Column(db.Date)
    Address = db.Column(db.String(200))
    Reg_date = db.Column(db.Date, nullable=False)
