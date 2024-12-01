4c112c39705fb05999c7253b0c6685c1fd62c30b
ilovemath25 <w3573y.young@gmail.com>
2024-12-01 09:16:58 +0800

    modify test data

--- DB_testdata.sql
+++ DB_testdata.sql
@@ -90,26 +90,26 @@ VALUES
 
 INSERT INTO "product" ("SMID", "SpEvent_ID", "Name", "Desc", "Author", "Price", "Stock_quantity", "Category", "Product_pict", "Sale_count") 
 VALUES
-  (2, 9,    "Harry Potter and the Sorcerer's Stone", "The beginning of the magical journey of Harry Potter, a young wizard discovering his powers and fighting the dark forces at Hogwarts.","J.K. Rowling",1500,10,NULL,"ebookstore_flask/static/image/books/PID_book1.png", 2),
-  (2, NULL, "The Lord of the Rings: The Fellowship of the Ring", "A fantasy epic about a young hobbit, Frodo, tasked with destroying a powerful ring to save Middle-earth.", "J.R.R. Tolkien",1200, 20,NULL,"ebookstore_flask/static/image/books/PID_book2.png", 1),
-  (9, NULL, "The Da Vinci Code" ,"A thrilling mystery that blends art, religion, and history in a race to uncover hidden secrets of the Church.", NULL , 800, 4,NULL,"bookstore_flask/static/image/books/PID_book3.png", 1),
-  (8, NULL, "The Great Gatsby", "A classic exploration of the American Dream, wealth, and unrequited love during the Roaring Twenties.", "F. Scott Fitzgerald", 900, 5,NULL,"ebookstore_flask/static/image/books/PID_book4.png", 4),
-  (8, NULL, "Pride and Prejudice","A beloved romance set in 19th-century England, highlighting societal expectations and enduring love.", "Jane Austen", 450, 30,NULL,"ebookstore_flask/static/image/books/PID_book5.png", 1),
-  (2, NULL, "To Kill a Mockingbird", "A timeless tale of racial injustice and moral growth in the American South.", "Harper Lee", 1320, 2,NULL,"ebookstore_flask/static/image/books/PID_book6.png", 1),
-  (9, NULL, "1984", "A dystopian masterpiece portraying a world dominated by surveillance, oppression, and propaganda.", "George Orwell", 1100, 1,NULL,"ebookstore_flask/static/image/books/PID_book7.png", 0),
-  (8, 9,    "The Catcher in the Rye", "A story of teenage angst and rebellion through the perspective of the iconic Holden Caulfield.", "J.D. Salinger", 1000, 0,NULL,"ebookstore_flask/static/image/books/PID_book8.png", 1),
-  (9, NULL, "Moby-Dick", "An adventurous tale of obsession, revenge, and the pursuit of the great white whale.", "Herman Melville", 870, 6,NULL,"ebookstore_flask/static/image/books/PID_book9.png", 0),
-  (9, NULL, "The Hobbit", "A prequel to 'The Lord of the Rings,' following Bilbo Baggins on his unexpected journey through Middle-earth.", "J.R.R. Tolkien",1400, 99, NULL,"ebookstore_flask/static/image/books/PID_book10.png", 5),
-  (2, NULL, "Animal Farm", "A satirical novella that uses a farm animal uprising to explore themes of power and corruption.", "George Orwell",890, 101 ,NULL,"ebookstore_flask/static/image/books/PID_book11.png", 1),
-  (9, 7,    "The Alchemist", "A journey of self-discovery and following one's dreams, told through the story of Santiago, a shepherd.", "Paulo Coelho", 1300, 80, NULL,"ebookstore_flask/static/image/books/PID_book12.png", 1),
-  (8, NULL, "Jane Eyre","A Gothic romance exploring love, independence, and resilience through the life of Jane Eyre.", "Charlotte Brontë", 990, 4,NULL,"ebookstore_flask/static/image/books/PID_book13.png", 0),
-  (2, NULL, "The Little Prince", "A poetic tale about love, loss, and imagination, narrated by a young prince from another planet.", "Antoine de Saint-Exupéry", 1000, 70, NULL,"ebookstore_flask/static/image/books/PID_book14.png", 7),
-  (8, NULL, "War and Peace", "A grand novel interweaving Russian society, history, and personal relationships during the Napoleonic Wars.", "Leo Tolstoy", 1111, 3, NULL,"ebookstore_flask/static/image/books/PID_book15.png", 1),
-  (8, NULL, "The Kite Runner", "A moving story of friendship, betrayal, and redemption set against the backdrop of Afghanistan.", "Khaled Hosseini", 1600, 0, NULL,"ebookstore_flask/static/image/books/PID_book16.png", 1),
-  (9, NULL, "The Fault in Our Stars", NULL, "John Green", 700,9 ,NULL,"ebookstore_flask/static/image/books/PID_book17.png", 0),
-  (2, NULL, "Sapiens: A Brief History of Humankind", "A compelling exploration of human evolution, culture, and the forces that shaped modern society.", "Yuval Noah Harari",650,11, NULL,"ebookstore_flask/static/image/books/PID_book18.png", 5),
-  (2, NULL, "The Hunger Games", "A dystopian novel about a teenage girl who becomes a symbol of rebellion in a world ruled by authoritarian control.", "Suzanne Collins", 750, 0,NULL,"ebookstore_flask/static/image/books/PID_book19.png", 1),
-  (9, NULL, "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe", "A magical adventure in the land of Narnia, where children discover their destiny to save a world in turmoil.", "C.S. Lewis", 1210, 100, NULL,"ebookstore_flask/static/image/books/PID_book20.png", 1);
+  (2, 9,    'Harry Potter and the Sorcerer''s Stone', 'The beginning of the magical journey of Harry Potter, a young wizard discovering his powers and fighting the dark forces at Hogwarts.','J.K. Rowling',1500,10,NULL,'ebookstore_flask/static/image/books/PID_book1.png', 2),
+  (2, NULL, 'The Lord of the Rings: The Fellowship of the Ring', 'A fantasy epic about a young hobbit, Frodo, tasked with destroying a powerful ring to save Middle-earth.', 'J.R.R. Tolkien',1200, 20,NULL,'ebookstore_flask/static/image/books/PID_book2.png', 1),
+  (9, NULL, 'The Da Vinci Code' ,'A thrilling mystery that blends art, religion, and history in a race to uncover hidden secrets of the Church.', NULL , 800, 4,NULL,'bookstore_flask/static/image/books/PID_book3.png', 1),
+  (8, NULL, 'The Great Gatsby', 'A classic exploration of the American Dream, wealth, and unrequited love during the Roaring Twenties.', 'F. Scott Fitzgerald', 900, 5,NULL,'ebookstore_flask/static/image/books/PID_book4.png', 4),
+  (8, NULL, 'Pride and Prejudice','A beloved romance set in 19th-century England, highlighting societal expectations and enduring love.', 'Jane Austen', 450, 30,NULL,'ebookstore_flask/static/image/books/PID_book5.png', 1),
+  (2, NULL, 'To Kill a Mockingbird', 'A timeless tale of racial injustice and moral growth in the American South.', 'Harper Lee', 1320, 2,NULL,'ebookstore_flask/static/image/books/PID_book6.png', 1),
+  (9, NULL, '1984', 'A dystopian masterpiece portraying a world dominated by surveillance, oppression, and propaganda.', 'George Orwell', 1100, 1,NULL,'ebookstore_flask/static/image/books/PID_book7.png', 0),
+  (8, 9,    'The Catcher in the Rye', 'A story of teenage angst and rebellion through the perspective of the iconic Holden Caulfield.', 'J.D. Salinger', 1000, 0,NULL,'ebookstore_flask/static/image/books/PID_book8.png', 1),
+  (9, NULL, 'Moby-Dick', 'An adventurous tale of obsession, revenge, and the pursuit of the great white whale.', 'Herman Melville', 870, 6,NULL,'ebookstore_flask/static/image/books/PID_book9.png', 0),
+  (9, NULL, 'The Hobbit', 'A prequel to ''The Lord of the Rings,'' following Bilbo Baggins on his unexpected journey through Middle-earth.', 'J.R.R. Tolkien',1400, 99, NULL,'ebookstore_flask/static/image/books/PID_book10.png', 5),
+  (2, NULL, 'Animal Farm', 'A satirical novella that uses a farm animal uprising to explore themes of power and corruption.', 'George Orwell',890, 101 ,NULL,'ebookstore_flask/static/image/books/PID_book11.png', 1),
+  (9, 7,    'The Alchemist', 'A journey of self-discovery and following one''s dreams, told through the story of Santiago, a shepherd.', 'Paulo Coelho', 1300, 80, NULL,'ebookstore_flask/static/image/books/PID_book12.png', 1),
+  (8, NULL, 'Jane Eyre','A Gothic romance exploring love, independence, and resilience through the life of Jane Eyre.', 'Charlotte Brontë', 990, 4,NULL,'ebookstore_flask/static/image/books/PID_book13.png', 0),
+  (2, NULL, 'The Little Prince', 'A poetic tale about love, loss, and imagination, narrated by a young prince from another planet.', 'Antoine de Saint-Exupéry', 1000, 70, NULL,'ebookstore_flask/static/image/books/PID_book14.png', 7),
+  (8, NULL, 'War and Peace', 'A grand novel interweaving Russian society, history, and personal relationships during the Napoleonic Wars.', 'Leo Tolstoy', 1111, 3, NULL,'ebookstore_flask/static/image/books/PID_book15.png', 1),
+  (8, NULL, 'The Kite Runner', 'A moving story of friendship, betrayal, and redemption set against the backdrop of Afghanistan.', 'Khaled Hosseini', 1600, 0, NULL,'ebookstore_flask/static/image/books/PID_book16.png', 1),
+  (9, NULL, 'The Fault in Our Stars', NULL, 'John Green', 700,9 ,NULL,'ebookstore_flask/static/image/books/PID_book17.png', 0),
+  (2, NULL, 'Sapiens: A Brief History of Humankind', 'A compelling exploration of human evolution, culture, and the forces that shaped modern society.', 'Yuval Noah Harari',650,11, NULL,'ebookstore_flask/static/image/books/PID_book18.png', 5),
+  (2, NULL, 'The Hunger Games', 'A dystopian novel about a teenage girl who becomes a symbol of rebellion in a world ruled by authoritarian control.', 'Suzanne Collins', 750, 0,NULL,'ebookstore_flask/static/image/books/PID_book19.png', 1),
+  (9, NULL, 'The Chronicles of Narnia: The Lion, the Witch and the Wardrobe', 'A magical adventure in the land of Narnia, where children discover their destiny to save a world in turmoil.', 'C.S. Lewis', 1210, 100, NULL,'ebookstore_flask/static/image/books/PID_book20.png', 1);
 
 INSERT INTO "ShoppingCart_item" ("CMID", "Tot_price")
 VALUES
@@ -153,12 +153,12 @@ VALUES
 
 INSERT INTO "Review" ("PID","MID","Time","Rate","Rev_text","Rev_picture","Rev_video","Reply_RID")
 VALUES
-  (1, 3,  '2024-10-13 14:23:00', 4, "An enthralling read with rich characters and a gripping plot. The author masterfully weaves themes of love, loss, and resilience into a story that lingers long after the final page.","ebookstore_flask/static/image/books/PID_book1.png", "ebookstore_flask/static/video/video1.mp4", NULL),
-  (2, 4,  '2024-11-14 09:15:00', 5, "A captivating journey through a richly imagined world. Highly recommended for fantasy lovers.","ebookstore_flask/static/image/books/PID_book2.png", NULL, NULL),
-  (3, 5,  '2024-10-02 16:45:00', 3, "An engaging plot, but the pacing felt uneven at times.","ebookstore_flask/static/image/books/PID_book3.png", NULL, NULL),
-  (4, 6,  '2024-10-28 11:20:00', 4, "Insightful and beautifully written. The author's storytelling is superb.","ebookstore_flask/static/image/books/PID_book4.png", "ebookstore_flask/static/video/video2.mp4", NULL),
-  (5, 7,  '2024-09-17 18:30:00', 2, "The story had potential, but the characters lacked depth.","ebookstore_flask/static/image/books/PID_book5.png", NULL, NULL),
-  (6, 10, '2024-11-26 14:00:00', 5, "An absolute masterpiece! This book will stay with me for a long time.","ebookstore_flask/static/image/books/PID_book6.png", "ebookstore_flask/static/video/video3.mp4", NULL),
-  (6, 2,  '2024-11-26 18:19:00', NULL, "Thank you for the reply!", NULL, NULL),
-  (1, 8,  '2024-10-15 20:59:00', NULL, "Thank you for the reply!", NULL, NULL),
-  (2, 9,  '2024-11-15 18:45:00', NULL, "Thank you for the reply!", NULL, NULL);
+  (1, 3,  '2024-10-13 14:23:00', 4, 'An enthralling read with rich characters and a gripping plot. The author masterfully weaves themes of love, loss, and resilience into a story that lingers long after the final page.','ebookstore_flask/static/image/books/PID_book1.png', 'ebookstore_flask/static/video/video1.mp4', NULL),
+  (2, 4,  '2024-11-14 09:15:00', 5, 'A captivating journey through a richly imagined world. Highly recommended for fantasy lovers.','ebookstore_flask/static/image/books/PID_book2.png', NULL, NULL),
+  (3, 5,  '2024-10-02 16:45:00', 3, 'An engaging plot, but the pacing felt uneven at times.','ebookstore_flask/static/image/books/PID_book3.png', NULL, NULL),
+  (4, 6,  '2024-10-28 11:20:00', 4, 'Insightful and beautifully written. The author''s storytelling is superb.','ebookstore_flask/static/image/books/PID_book4.png', 'ebookstore_flask/static/video/video2.mp4', NULL),
+  (5, 7,  '2024-09-17 18:30:00', 2, 'The story had potential, but the characters lacked depth.','ebookstore_flask/static/image/books/PID_book5.png', NULL, NULL),
+  (6, 10, '2024-11-26 14:00:00', 5, 'An absolute masterpiece! This book will stay with me for a long time.','ebookstore_flask/static/image/books/PID_book6.png', 'ebookstore_flask/static/video/video3.mp4', NULL),
+  (6, 2,  '2024-11-26 18:19:00', NULL, 'Thank you for the reply!', NULL, NULL),
+  (1, 8,  '2024-10-15 20:59:00', NULL, 'Thank you for the reply!', NULL, NULL),
+  (2, 9,  '2024-11-15 18:45:00', NULL, 'Thank you for the reply!', NULL, NULL);
