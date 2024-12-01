873ed1c608e6ccca7ccae53406048ba0d2c54b52
ilovemath25 <w3573y.young@gmail.com>
2024-12-01 14:58:03 +0800

    modify product.py

--- ebookstore_flask/models/product.py
+++ ebookstore_flask/models/product.py
@@ -1,15 +1,15 @@
 from . import db
+
 class Product(db.Model):
     __tablename__ = 'Product'
-    PID = db.Column(db.Integer, primary_key=True, nullable=False)  
-    SMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)  
-    SpEvent_ID = db.Column(db.Integer, db.ForeignKey('Special_event.DID', onupdate="CASCADE", ondelete="CASCADE")) 
-    Name = db.Column(db.String(50), nullable=False)  
-    Desc = db.Column(db.String(1000))  
-    Author = db.Column(db.String(30))  
-    Price = db.Column(db.Integer, nullable=False)  
-    Stock_quantity = db.Column(db.Integer, nullable=False)  
-    Category = db.Column(db.String(20)) 
-    Product_pict = db.Column(db.String(100)) 
-    Sale_count = db.Column(db.Integer)
-    
+    PID = db.Column(db.Integer, primary_key=True, nullable=False)
+    SMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    SpEvent_ID = db.Column(db.Integer, db.ForeignKey('Special_event.DID', onupdate="CASCADE", ondelete="SET NULL"))
+    Name = db.Column(db.String(100), nullable=False)
+    Desc = db.Column(db.String(1000))
+    Author = db.Column(db.String(50))
+    Price = db.Column(db.Integer, nullable=False)
+    Stock_quantity = db.Column(db.Integer, nullable=False)
+    Category = db.Column(db.String(20))
+    Product_pict = db.Column(db.String(100))
+    Sale_count = db.Column(db.Integer, nullable=False)
