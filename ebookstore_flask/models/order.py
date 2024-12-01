362b91346aa8e1a9b706b42bae8a448ad9668c3f
ilovemath25 <w3573y.young@gmail.com>
2024-12-01 14:56:34 +0800

    modify order.py

--- ebookstore_flask/models/order.py
+++ ebookstore_flask/models/order.py
@@ -1,14 +1,15 @@
 from . import db
+
 class Order(db.Model):
    __tablename__ = 'Order'
    OID = db.Column(db.Integer, primary_key=True, nullable=False)
    CMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
    SMID = db.Column(db.Integer, db.ForeignKey('Member.MID', onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
    DID = db.Column(db.Integer, db.ForeignKey('Discount.DID', onupdate="CASCADE", ondelete="SET NULL"))
-   Credit_num = db.Column(db.String(16), db.ForeignKey('Credit_card.Number', onupdate="CASCADE", ondelete="CASCADE"))
+   Credit_num = db.Column(db.String(16), db.ForeignKey('Credit_card.Number', onupdate="CASCADE", ondelete="SET NULL"))
    Time = db.Column(db.DateTime, nullable=False)
    Ship_address = db.Column(db.String(200), nullable=False)
    Ship_fee = db.Column(db.Integer, nullable=False)
-   Status = db.Column(db.String(15), db.Enum('Received','Processing','Shipping','Closed'), nullable=False)
-   Pay_method = db.Column(db.String(15), db.Enum('Credit card','COD'), nullable=False)
+   Status = db.Column(db.Enum('Processing', 'Shipping', 'Received', 'Closed'), nullable=False)
+   Pay_method = db.Column(db.Enum('Credit card', 'COD'), nullable=False)
    Tot_price = db.Column(db.Integer)
