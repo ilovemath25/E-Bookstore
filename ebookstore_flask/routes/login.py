529a7d389312fdd9ee24c6156141d0d3c9398219
ilovemath25 <w3573y.young@gmail.com>
2024-12-20 08:11:27 +0800

    "modify login and add comment at home"

--- ebookstore_flask/routes/login.py
+++ ebookstore_flask/routes/login.py
@@ -1,22 +1,23 @@
 from flask import Blueprint, render_template, request, redirect, url_for, make_response
 from models import db
-from utils import session
 login = Blueprint('login', __name__)
 
-@login.route('/login', methods=['POST', 'GET'])
+@login.route('/login_email', methods=['POST', 'GET'])
 def index():
    if request.method == 'POST':
+      from ebookstore_flask.models.member import Member
+      from utils.session import add_session, check_role
       email = request.form.get('email')
       password = request.form.get('password')
-      query = db.session.execute('''
-         SELECT *
-         FROM MEMBER
-         WHERE "Email" = :email;
-      ''', {'email': email})
-      user = query.fetchone()
+      user = (
+         Member.query                     # SELECT * FROM "Member"
+         .filter(Member.Email == email)   # WHERE "Email" = <email>;
+         .all()
+      )
       if user:
          if user[3]==password:
-            session_id = session.add_session(user[5], user[0])
+            role = check_role(user[10], user[11], user[12])
+            session_id = add_session(user[5], user[0], role)
             resp = make_response(redirect(url_for('home.index')))
             resp.set_cookie('session_id', session_id, max_age=60*60*24)
             next_url = request.args.get('next')
--- ebookstore_flask/utils/session.py
+++ ebookstore_flask/utils/session.py
@@ -3,13 +3,19 @@ from hashlib import sha256
 import json
 from datetime import datetime
 
+def check_role(C_flag, S_flag, A_flag):
+   if sum([C_flag, S_flag, A_flag]) > 1: raise ValueError("Error: Multiple roles selected")
+   if C_flag: return "Customer"
+   elif S_flag: return "Staff"
+   elif A_flag: return "Administrator"
+   
 def load_sessions():
    with open("sessions.json", 'r') as f: return json.load(f)
 
-def add_session(email, mid):
+def add_session(email, mid, role):
    session_store = load_sessions()
    session_id = generate_session(email, mid)
-   session_store[session_id] = email
+   session_store[session_id] = [email, role]
    with open("sessions.json", 'w') as f: json.dump(session_store, f, indent=4)
    return session_id
 
