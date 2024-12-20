f72156e35a5e4d60ee475aa2fbd795dda4dfad3f
jeon0121 <jamie0121304@gmail.com>
2024-12-20 17:04:15 +0800

    Add file

--- ebookstore_flask/routes/home.py
+++ ebookstore_flask/routes/home.py
@@ -8,35 +8,36 @@ def index():
    from ebookstore_flask.models.product import Product
    from ebookstore_flask.models.review import Review
    from ebookstore_flask import db
+   # .all() must be added in all query
+   # it used to convert SQL query object into python list
    best_seller = (
-      Product.query
-      .order_by(Product.Sale_count.desc())
-      .limit(10)
+      Product.query                         # SELECT * FROM "Product"
+      .order_by(Product.Sale_count.desc())  # ORDER BY "Sale_count" DESC
+      .limit(10)                            # LIMIT 10;
       .all()
    )
    
    new_release = (
-      Product.query
-      .order_by(Product.PID.desc())
-      .limit(10)
+      Product.query                  # SELECT * FROM "Product"
+      .order_by(Product.PID.desc())  # ORDER BY "PID" DESC
+      .limit(10)                     # LIMIT 10;
       .all()
    )
    
    top_5_category = (
-      Product.query
-      .with_entities(Product.Category)
-      .group_by(Product.Category)
-      .order_by(db.func.sum(Product.Sale_count).desc())
-      .limit(5)
+      Product.query.with_entities(Product.Category)      # SELECT "Category" FROM "Product"
+      .group_by(Product.Category)                        # GROUP BY "Category"
+      .order_by(db.func.sum(Product.Sale_count).desc())  # ORDER BY SUM("Sale_count")
+      .limit(5)                                          # LIMIT 5;
       .all()
    )
-   top_5_category = [category[0] for category in top_5_category]
+   top_5_category = [category[0] for category in top_5_category]  # because query result is [(category1,), (category2,), (category3,)]
    
    top_rated = (
-      db.session.query(Product, Review.Rate)
-      .filter(Product.PID == Review.PID)
-      .filter(Review.Rate.isnot(None))
-      .order_by(Review.Rate.desc())
+      db.session.query(Product, Review.Rate) # SELECT "Product".*, "Review"."Rate" FROM "Product", "Review"
+      .filter(Product.PID == Review.PID)     # WHERE "Product"."PID" = "Review"."PID"
+      .filter(Review.Rate.isnot(None))       #   AND "Review"."Rate" IS NOT NULL
+      .order_by(Review.Rate.desc())          # ORDER BY "Review"."Rate" DESC
       .all()
    )
    
