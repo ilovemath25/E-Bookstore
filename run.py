3f5d4a61175e953ea3184be98b58ad693d31c59f
feliciarulita <feliciarulita@gmail.com>
2024-12-21 13:42:53 +0800

    modify order list

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from ebookstore_flask import create_app
 POSTGRES = {
    'user':'postgres',
-   'password':'postgres',
+   'password':'FeFe7404',
    'db':'ebookstore',
    'host':'localhost',
-   'port':'5433',
+   'port':'5432',
 }
 app = create_app(POSTGRES)
 if __name__=='__main__':app.run(host='0.0.0.0', debug=True, port=5001)
