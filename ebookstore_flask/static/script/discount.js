5bdc90ca14b656ff446264ba0ef6fe2066efef82
jeon0121 <jamie0121304@gmail.com>
2024-12-27 18:12:27 +0800

    fix discount radio

--- ebookstore_flask/static/script/discount.js
+++ ebookstore_flask/static/script/discount.js
@@ -1,16 +1,18 @@
 function toggleFields(selectedType) {
     const dateFields = document.getElementById('date-fields');
     const shippingFields = document.getElementById('shipping-fields');
-
     if (selectedType === 'Shipping') {
         dateFields.style.display = 'none';
         shippingFields.style.display = 'block';
-    } else {
+    }
+    else if (selectedType === 'Seasoning' || selectedType === 'Special Event'){
         dateFields.style.display = 'block';
         shippingFields.style.display = 'none';
     }
 }
 document.addEventListener('DOMContentLoaded', function () {
-    const selectedType = "{{ discount.Disc_type }}";
-    toggleFields(selectedType);
+    const radioInput = document.querySelectorAll(".radio-option input");
+    if (radioInput[0].checked) toggleFields("Shipping");
+    else if (radioInput[1].checked) toggleFields("Seasoning");
+    else if (radioInput[2].checked) toggleFields("Special Event");
 });
