5f220cda121b55dc05c4069c4592b28b91630d51
jeon0121 <jamie0121304@gmail.com>
2024-12-27 23:34:25 +0800

    fix space

--- ebookstore_flask/static/script/order.js
+++ ebookstore_flask/static/script/order.js
@@ -5,29 +5,29 @@ document.querySelector('.selected-filter').addEventListener('click', function(){
  });
  document.querySelectorAll('.option-filter p').forEach(function(option){
     option.addEventListener('click', function(){
-       var selectedLang = this.textContent;
-       var optionDiv = document.querySelector('.option-filter');
-       var selectedText = document.getElementById('selectedText');
-        //    document.querySelector('.selected-filter').textContent = selectedLang;
-       selectedText.textContent = selectedLang;
-       localStorage.setItem("option_filter", selectedText.textContent);
-       document.querySelector('.selected-filter').style.color = "black";
-       optionDiv.style.display = 'none';
-       if (selectedLang === 'Discount ID'|| selectedLang === 'Discount Code'){
-         selectedLang = 'Order ID'
-      }
-       document.getElementById('filter_field').value = selectedLang === 'Product' ? 'product' : 'order_id';
+        var selectedLang = this.textContent;
+        var optionDiv = document.querySelector('.option-filter');
+        var selectedText = document.getElementById('selectedText');
+            //    document.querySelector('.selected-filter').textContent = selectedLang;
+        selectedText.textContent = selectedLang;
+        localStorage.setItem("option_filter", selectedText.textContent);
+        document.querySelector('.selected-filter').style.color = "black";
+        optionDiv.style.display = 'none';
+        if (selectedLang === 'Discount ID'|| selectedLang === 'Discount Code'){
+            selectedLang = 'Order ID'
+        }
+        document.getElementById('filter_field').value = selectedLang === 'Product' ? 'product' : 'order_id';
     });
  });
  document.addEventListener("DOMContentLoaded", () => {
     var selectedText = document.getElementById('selectedText');
     var savedOption = localStorage.getItem("option_filter");
     if (savedOption) {
-       if (savedOption === 'Discount ID'|| savedOption === 'Discount Code'){
-          savedOption = 'Order ID'
-         }
-       selectedText.textContent = savedOption;
-        document.getElementById('filter_field').value = savedOption === 'Product' ? 'product' : 'order_id';
+        if (savedOption === 'Discount ID'|| savedOption === 'Discount Code'){
+            savedOption = 'Order ID'
+        }
+        selectedText.textContent = savedOption;
+            document.getElementById('filter_field').value = savedOption === 'Product' ? 'product' : 'order_id';
     }
     // if (selectedText.innerHTML == "Order ID") {
     //     selectedText.innerHTML = localStorage.getItem("option_filter");
@@ -41,7 +41,7 @@ document.querySelector('.selected-filter').addEventListener('click', function(){
  function selectOption(optionText) {
     const selectedText = document.getElementById('selectedText');
     if (optionText === 'Discount ID'|| optionText === 'Discount Code'){
-      optionText = 'Order ID'
+        optionText = 'Order ID'
   }
     selectedText.textContent = optionText;
 }
