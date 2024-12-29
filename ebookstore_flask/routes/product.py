5232b8783c9519d10c3dede11ee183f5aaa84e4f
ilovemath25 <w3573y.young@gmail.com>
2024-12-29 18:23:59 +0800

    Add session role handling in product index and adjust login form margins

--- ebookstore_flask/routes/product.py
+++ ebookstore_flask/routes/product.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from ebookstore_flask.utils.session import check_session
 
 product = Blueprint('product', __name__)
 
@@ -6,7 +7,11 @@ product = Blueprint('product', __name__)
 def index(product_id):
    from ebookstore_flask.models.product import Product
    product = Product.query.get(product_id)
+   role = None
+   session_data = check_session()
+   if(session_data): _, role = session_data
    return render_template(
       "/user/product.html",
-      product=product
+      product=product,
+      role=role
    )
