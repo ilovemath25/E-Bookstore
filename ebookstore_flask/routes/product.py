68c0e54f73c903cf6d03c1f4591d9cf18a8ac93a
ilovemath25 <w3573y.young@gmail.com>
2024-12-29 17:35:41 +0800

    modify image source

--- ebookstore_flask/routes/product.py
+++ ebookstore_flask/routes/product.py
@@ -6,9 +6,6 @@ product = Blueprint('product', __name__)
 def index(product_id):
    from ebookstore_flask.models.product import Product
    product = Product.query.get(product_id)
-   if product:
-      if product.Product_pict.startswith('ebookstore_flask/'):product.Product_pict = product.Product_pict.replace('ebookstore_flask/', '')
-      if product.Product_pict.startswith('static/'):product.Product_pict = product.Product_pict.replace('static/', '')
    return render_template(
       "/user/product.html",
       product=product
