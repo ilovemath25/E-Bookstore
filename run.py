8652978b446ff0fe3422855841b83b41a1b22e4e
vintbry <vincentius8055@gmail.com>
2024-12-31 15:03:34 +0800

    modify order user profile

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ POSTGRES = {
    'password':'haloinibv',
    'db':'e-bookstore',
    'host':'localhost',
-   'port':'5433',
+   'port':'5432',
 }
 app = create_app(POSTGRES)
 if __name__=='__main__':app.run(host='0.0.0.0', debug=True, port=5001)
