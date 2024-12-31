dea634cfa2b628f810449e5652f8bda4fb821de0
hao <thihakyaw148@gmail.com>
2024-12-31 14:15:50 +0800

    modify users_edit

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from ebookstore_flask import create_app
 POSTGRES = {
    'user':'postgres',
-   'password':'FeFe7404',
-   'db':'ebookstore',
+   'password':'thiha2001',
+   'db':'postgres',
    'host':'localhost',
    'port':'5432',
 }
